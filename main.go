4a70dee36eef887f438510e714e756342954edf5
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2019-11-13 22:18:01 -0500

    refactor

--- main.go
+++ main.go
@@ -19,36 +19,44 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+//Google cloud functions entrypoint
 func Handler(w http.ResponseWriter, r *http.Request) {
 	bytes, _ := ioutil.ReadAll(r.Body)
 	event, e := slackevents.ParseEvent(bytes, slackevents.OptionNoVerifyToken())
 	if e != nil {
 		fmt.Println("error: ", e)
 	}
-	//initial slack endpoint verification, only happens on install
-	if event.Type == slackevents.URLVerification {
-		var r *slackevents.ChallengeResponse
-		err := json.Unmarshal(bytes, &r)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-		w.Header().Set("Content-Type", "text")
-		w.Write([]byte(r.Challenge))
+	switch event.Type {
+	case slackevents.URLVerification:
+		verifyUrl(w, bytes)
+	case slackevents.CallbackEvent:
+		handleEvent(event.InnerEvent)
 	}
-	//there's actually a message to handle
-	if event.Type == slackevents.CallbackEvent {
-		innerEvent := event.InnerEvent
-		switch ev := innerEvent.Data.(type) {
-		case *slackevents.MessageEvent:
-			if roll() || ev.ChannelType == "im" {
-				_ = api.AddReaction("chicken", slack.NewRefToMessage(ev.Channel, ev.TimeStamp))
-			}
-		case *slackevents.AppMentionEvent:
+}
+
+//We got an actual event of some kind, let's (maybe) do something stupid
+func handleEvent(ie slackevents.EventsAPIInnerEvent) {
+	switch ev := ie.Data.(type) {
+	case *slackevents.MessageEvent:
+		if roll() || ev.ChannelType == "im" {
 			_ = api.AddReaction("chicken", slack.NewRefToMessage(ev.Channel, ev.TimeStamp))
 		}
+	case *slackevents.AppMentionEvent:
+		_ = api.AddReaction("chicken", slack.NewRefToMessage(ev.Channel, ev.TimeStamp))
 	}
 }
 
 func roll() bool {
 	return rand.Intn(20) < 1
+}
+
+//Handle an initial slack verification request... Should only happen on install
+func verifyUrl(w http.ResponseWriter, bytes []byte) {
+	var r *slackevents.ChallengeResponse
+	err := json.Unmarshal(bytes, &r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "text")
+	_, _ = w.Write([]byte(r.Challenge))
 }
